6d4fc397d3dccfcef956166bf5f59ffd7c8f1cf8
Michael Roberts <mikedroberts88@gmail.com>
2022-10-14 08:45:01 -0500

    Added Icategory Interface & Category Repository
    Added Icategory Interface & Category Repository

---
+++ Services/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using BookApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApiProject.Services
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private BookDbContext _categoryContext;
+
+        public CategoryRepository(BookDbContext categoryContext)
+        {
+            _categoryContext = categoryContext;
+        }
+
+        public bool CategoryExists(int catergoryId)
+        {
+            return _categoryContext.Categories.Any(c => c.Id == catergoryId);
+        }
+
+        public ICollection<Book> GetAllBooksForCategory(int categoryId)
+        {
+            return _categoryContext.BookCategories.Where(c => c.CategoryId == categoryId).Select(b => b.Book).ToList();
+        }
+
+        public ICollection<Category> GetAllCategoriesForABook(int bookId)
+        {
+            return _categoryContext.BookCategories.Where(b => b.BookId == bookId).Select(c => c.Category).ToList();  // Accessing Intermediary Table to get category in bookId
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _categoryContext.Categories.OrderBy(c => c.Name).ToList();
+        }
+
+        public Category GetCategory(int categoryId)
+        {
+            return _categoryContext.Categories.Where(c => c.Id == categoryId).FirstOrDefault();  //Filter to get categoryID match
+        }
+    }
+}
---
+++ Services/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookApiProject.Models;
+
+namespace BookApiProject.Services
+{
+    public interface ICategoryRepository
+    {
+
+        ICollection<Category> GetCategories(); // Returns all categories 
+        Category GetCategory(int categoryId);  // returns single category
+        ICollection<Category> GetAllCategoriesForABook(int bookId); // Gets specific category for book
+        ICollection<Book> GetAllBooksForCategory(int categoryId); // retrieve all book categories from specific books
+
+        bool CategoryExists(int catergoryId); // Input validation To confirm that the country exists
+    }
+}
